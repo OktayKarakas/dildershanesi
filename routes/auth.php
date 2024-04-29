382d997a46ed83a2483650f4d36954f6f54e9d41
Oktay Karakaş <109868198+OktayKarakas@users.noreply.github.com>
2024-04-29 04:20:10 +0300

    cache update

--- routes/auth.php
+++ routes/auth.php
@@ -30,13 +30,6 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-                ->name('verification.notice');
-
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['signed', 'throttle:6,1'])
-                ->name('verification.verify');
-
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
                 ->middleware('throttle:6,1')
                 ->name('verification.send');
