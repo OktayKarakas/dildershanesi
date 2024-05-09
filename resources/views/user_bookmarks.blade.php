3b55faea20782c34b0b56e1b9f02fd48d3451f16
Oktay Karakaş <109868198+OktayKarakas@users.noreply.github.com>
2024-05-09 19:33:54 +0300

    remove konu from beğenilenler

--- resources/views/user_bookmarks.blade.php
+++ resources/views/user_bookmarks.blade.php
@@ -25,9 +25,6 @@
                             <p class="text-sm text-gray-500">
                                Topic: {{$topic->title}}
                             </p>
-                            <p class="text-sm text-gray-500">
-                                Konu: {!! $konu_anlatimi->title !!}
-                            </p>
                         </div>
                         <div class="ps-3">
                             <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
