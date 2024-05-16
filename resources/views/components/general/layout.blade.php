bedca4138facce51ca878ae95f4fbbfda370e8dc
Oktay Karakaş <109868198+OktayKarakas@users.noreply.github.com>
2024-05-16 05:54:04 +0300

    adsense

--- resources/views/components/general/layout.blade.php
+++ resources/views/components/general/layout.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2444807945895967"
+            crossorigin="anonymous"></script>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
